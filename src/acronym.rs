c6da9493fb2f2ff4a4b864fa9a9fa709538639db
Curtis McEnroe <programble@gmail.com>
2015-08-06 00:09:35 -0400

    Create Acronym struct

--- src/acronym.rs
+++ src/acronym.rs
@@ -1,5 +1,6 @@
 //! Acronym and Word model.
 
+use std::ops::Add;
 use std::ascii::AsciiExt;
 
 /// A word in an acronym, which can have zero or more of its initial letters appear.
@@ -10,7 +11,7 @@ pub struct Word {
 }
 
 impl Word {
-    /// Creates a new `Word` showing its first letter.
+    /// Creates a new `Word` with its first letter visible.
     pub fn new(s: String) -> Word {
         Word {
             string: s,
@@ -18,20 +19,7 @@ impl Word {
         }
     }
 
-    /// Creates an uppercase `String` of the initial letters shown for this word.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use tbd::acronym::Word;
-    /// let w = Word::new("example".to_string());
-    /// assert_eq!("E", w.initial());
-    /// ```
-    pub fn initial(&self) -> String {
-        self.string[0..self.initial_count].to_ascii_uppercase()
-    }
-
-    /// Sets the number of initials to show for this word.
+    /// Sets the number of visible initial letters.
     ///
     /// # Panics
     ///
@@ -41,17 +29,90 @@ impl Word {
     ///
     /// ```
     /// use tbd::acronym::Word;
-    /// let mut w = Word::new("example".to_string());
-    /// w.set_initial_count(2);
+    /// let mut w = Word::new(String::from("example")).with_count(2);
     /// assert_eq!("EX", w.initial());
     /// ```
-    pub fn set_initial_count(&mut self, count: usize) {
+    pub fn with_count(mut self, count: usize) -> Self {
         assert!(count <= self.string.len());
         self.initial_count = count;
+        self
+    }
+
+    /// Returns the number of visible initial letters.
+    pub fn initial_count(&self) -> usize {
+        self.initial_count
+    }
+
+    /// Creates an uppercase `String` of the visible initial letters for this word.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tbd::acronym::Word;
+    /// let w = Word::new("example".to_string());
+    /// assert_eq!("E", w.initial());
+    /// ```
+    pub fn initial(&self) -> String {
+        self.string[..self.initial_count].to_ascii_uppercase()
     }
 
     /// Returns the word length.
     pub fn len(&self) -> usize {
         self.string.len()
+    }
+}
+
+/// An acronym.
+///
+/// # Examples
+///
+/// ```
+/// use tbd::acronym::{Word, Acronym};
+///
+/// let a = Acronym {
+///     words: vec![
+///         Word::new(String::from("Great")),
+///         Word::new(String::from("Acronym")),
+///         Word::new(String::from("Example")),
+///     ],
+/// };
+///
+/// assert_eq!("GAE", a.to_string());
+/// ```
+#[derive(Clone, Debug)]
+pub struct Acronym {
+    pub words: Vec<Word>,
+}
+
+impl Acronym {
+    /// Returns the acronym length.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tbd::acronym::{Word, Acronym};
+    ///
+    /// let a = Acronym {
+    ///     words: vec![
+    ///         Word::new(String::from("Great")).with_count(2),
+    ///         Word::new(String::from("Acronym")),
+    ///         Word::new(String::from("Example")).with_count(3),
+    ///     ],
+    /// };
+    ///
+    /// assert_eq!(6, a.len());
+    /// ```
+    pub fn len(&self) -> usize {
+        self.words.iter()
+            .map(Word::initial_count)
+            .fold(0, usize::add)
+    }
+}
+
+impl ToString for Acronym {
+    fn to_string(&self) -> String {
+        self.words.iter()
+            .map(Word::initial)
+            .fold(String::new(), |s, i| s + &i)
     }
 }
