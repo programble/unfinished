f3e867ab88c6c06add1dad3c20f89e70d0c4518f
Curtis McEnroe <programble@gmail.com>
2016-07-23 20:54:23 -0400

    Enable warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+//! Link header (de)serialization.
+//!
+//! Implementation of [RFC 5988](https://tools.ietf.org/html/rfc5988).
+
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    variant_size_differences,
+)]
