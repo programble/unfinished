376e7c3dd065f325b52ded5df0e9d5aea89b36ce
Curtis McEnroe <programble@gmail.com>
2016-09-05 19:07:08 -0400

    Add SI prefix aliases

--- src/aliases.rs
+++ src/aliases.rs
@@ -105,3 +105,75 @@ pub type FixI64D<E> = FixI64<U10, E>;
 
 /// Fixed-point `isize`, scaled in decimal.
 pub type FixIsizeD<E> = FixIsize<U10, E>;
+
+/// SI prefixes.
+pub mod si {
+    use typenum::{N1, N2, N3, N6, N9, N12, N15, N18, N21, N24};
+    use typenum::{P1, P2, P3, P6, P9, P12, P15, P18, P21, P24};
+    use typenum::{U10, Z0};
+
+    use Fix;
+
+    /// 10^-24
+    pub type Yocto<N> = Fix<N, U10, N24>;
+
+    /// 10^-21
+    pub type Zepto<N> = Fix<N, U10, N21>;
+
+    /// 10^-18
+    pub type Atto<N> = Fix<N, U10, N18>;
+
+    /// 10^-15
+    pub type Femto<N> = Fix<N, U10, N15>;
+
+    /// 10^-12
+    pub type Pico<N> = Fix<N, U10, N12>;
+
+    /// 10^-9
+    pub type Nano<N> = Fix<N, U10, N9>;
+
+    /// 10^-6
+    pub type Micro<N> = Fix<N, U10, N6>;
+
+    /// 10^-3
+    pub type Milli<N> = Fix<N, U10, N3>;
+
+    /// 10^-2
+    pub type Centi<N> = Fix<N, U10, N2>;
+
+    /// 10^-1
+    pub type Deci<N> = Fix<N, U10, N1>;
+
+    /// 10^0
+    pub type Unit<N> = Fix<N, U10, Z0>;
+
+    /// 10^1
+    pub type Deca<N> = Fix<N, U10, P1>;
+
+    /// 10^2
+    pub type Hecto<N> = Fix<N, U10, P2>;
+
+    /// 10^3
+    pub type Kilo<N> = Fix<N, U10, P3>;
+
+    /// 10^6
+    pub type Mega<N> = Fix<N, U10, P6>;
+
+    /// 10^9
+    pub type Giga<N> = Fix<N, U10, P9>;
+
+    /// 10^12
+    pub type Tera<N> = Fix<N, U10, P12>;
+
+    /// 10^15
+    pub type Peta<N> = Fix<N, U10, P15>;
+
+    /// 10^18
+    pub type Exa<N> = Fix<N, U10, P18>;
+
+    /// 10^21
+    pub type Zeta<N> = Fix<N, U10, P21>;
+
+    /// 10^24
+    pub type Yotta<N> = Fix<N, U10, P24>;
+}
