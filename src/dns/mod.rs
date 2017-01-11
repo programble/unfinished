42a4947d5027a43c0c86562e69b39b367f1b6cab
Curtis McEnroe <programble@gmail.com>
2017-01-10 23:43:15 -0500

    Add traits QType, Type, QClass, Class

--- src/dns/mod.rs
+++ src/dns/mod.rs
@@ -7,3 +7,33 @@ mod name;
 pub use self::error::Error;
 pub use self::label::Label;
 pub use self::name::Name;
+
+/// Resource record query types.
+///
+/// Superset of [`Type`](trait.Type.html).
+pub trait QType {
+    /// Returns the type code value.
+    ///
+    /// Wants to be an associated constant.
+    fn value() -> u16;
+}
+
+/// Resource record types.
+///
+/// Subset of [`QType`](trait.QType.html).
+pub trait Type: QType { }
+
+/// Resource record query classes.
+///
+/// Superset of [`Class`](trait.Class.html).
+pub trait QClass {
+    /// Returns the class code value.
+    ///
+    /// Wants to be an associated constant.
+    fn value() -> u16;
+}
+
+/// Resource record classes.
+///
+/// Subset of [`QClass`](trait.QClass.html).
+pub trait Class: QClass { }
