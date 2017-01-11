cdbcd99549831e36b32f9d466dbbe07cbe507792
Curtis McEnroe <programble@gmail.com>
2017-01-10 23:02:41 -0500

    Add Deserialize::from_bytes

--- src/s11n.rs
+++ src/s11n.rs
@@ -6,13 +6,13 @@ use byteorder::{ReadBytesExt, WriteBytesExt};
 
 /// Serialize to a `Write`.
 pub trait Serialize {
-    /// Perform the serialization.
+    /// Performs the serialization.
     fn serialize<W: WriteBytesExt>(&self, write: &mut W) -> Result<(), IoError>;
 
     /// Returns the length in bytes of the serialization.
     fn serialized_len(&self) -> usize;
 
-    /// Serialize to a `Vec<u8>`.
+    /// Serializes to a `Vec<u8>`.
     fn to_bytes(&self) -> Vec<u8> {
         let mut vec = Vec::with_capacity(self.serialized_len());
         self.serialize(&mut vec).unwrap();
@@ -25,6 +25,11 @@ pub trait Deserialize: Sized {
     /// The type returned in the event of a deserialization error.
     type Err;
 
-    /// Perform the deserialization.
+    /// Performs the deserialization.
     fn deserialize<R: ReadBytesExt>(read: &mut R) -> Result<Self, Self::Err>;
+
+    /// Deserializes from a `&[u8]`.
+    fn from_bytes(mut bytes: &[u8]) -> Result<Self, Self::Err> {
+        Self::deserialize(&mut bytes)
+    }
 }
