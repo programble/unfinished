2b0f256e576ae0c5e0a107a391439c44c367dc1c
Curtis McEnroe <programble@gmail.com>
2017-02-12 15:47:26 -0500

    Export prefix bytes from encode::prefix module

--- src/encode/prefix.rs
+++ src/encode/prefix.rs
@@ -1,10 +1,22 @@
 use super::Encode;
+
 use mnemonic::prefix::{Lock, Rep, Repne};
 use output::Output;
 
-const LOCK: u8 = 0xf0;
-const REPNE: u8 = 0xf2;
-const REP: u8 = 0xf3;
+pub const LOCK: u8 = 0xf0;
+pub const REPNE: u8 = 0xf2;
+pub const REP: u8 = 0xf3;
+
+pub const CS: u8 = 0x2e;
+pub const SS: u8 = 0x36;
+pub const DS: u8 = 0x3e;
+pub const ES: u8 = 0x26;
+pub const FS: u8 = 0x64;
+pub const GS: u8 = 0x65;
+
+pub const OPERAND_SIZE: u8 = 0x66;
+
+pub const ADDRESS_SIZE: u8 = 0x67;
 
 impl<I> Encode for Lock<I> where I: Encode {
     fn encode<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
