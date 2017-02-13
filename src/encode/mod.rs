f51354884e58c12a46aaf782ee29544730122fb5
Curtis McEnroe <programble@gmail.com>
2017-02-13 17:05:06 -0500

    Shorten Encode trait definition

--- src/encode/mod.rs
+++ src/encode/mod.rs
@@ -6,43 +6,25 @@ use output::Output;
 
 #[allow(unused_variables)]
 pub trait Encode {
-    fn encode_prefix1<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
-        Ok(())
-    }
+    fn encode_prefix1<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output { Ok(()) }
 
-    fn encode_prefix2<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
-        Ok(())
-    }
+    fn encode_prefix2<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output { Ok(()) }
 
-    fn encode_prefix3<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
-        Ok(())
-    }
+    fn encode_prefix3<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output { Ok(()) }
 
-    fn encode_prefix4<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
-        Ok(())
-    }
+    fn encode_prefix4<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output { Ok(()) }
 
-    fn encode_rex<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
-        Ok(())
-    }
+    fn encode_rex<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output { Ok(()) }
 
     fn encode_opcode<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output;
 
-    fn encode_modrm<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
-        Ok(())
-    }
+    fn encode_modrm<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output { Ok(()) }
 
-    fn encode_sib<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
-        Ok(())
-    }
+    fn encode_sib<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output { Ok(()) }
 
-    fn encode_disp<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
-        Ok(())
-    }
+    fn encode_disp<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output { Ok(()) }
 
-    fn encode_imm<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
-        Ok(())
-    }
+    fn encode_imm<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output { Ok(()) }
 
     fn encode<O>(&self, out: &mut O) -> Result<(), O::Err> where O: Output {
         self.encode_prefix1(out)?;
