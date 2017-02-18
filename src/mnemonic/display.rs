adad8f5073ef670a0000743f09cccac7145a717d
Curtis McEnroe <programble@gmail.com>
2017-02-18 13:41:11 -0500

    Implement Display for Memory

--- src/mnemonic/display.rs
+++ src/mnemonic/display.rs
@@ -7,8 +7,8 @@ use core::fmt::{Display, Formatter, Error};
 macro_rules! impl_display_reg {
     ($reg:ident { $($var:ident => $str:expr,)+ }) => {
         impl Display for $reg {
-            fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
-                fmt.write_str(
+            fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+                f.write_str(
                     match *self {
                         $($reg::$var => $str),+
                     }
@@ -153,3 +153,126 @@ impl_display_reg!(
         R15 => "r15",
     }
 );
+
+impl Display for Disp {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match *self {
+            Disp::Disp8(disp) => write!(f, "{:#02x}", disp),
+            Disp::Disp32(disp) => write!(f, "{:#08x}", disp),
+        }
+    }
+}
+
+impl Display for Scale {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match *self {
+            Scale::X1 => f.write_str("1"),
+            Scale::X2 => f.write_str("2"),
+            Scale::X4 => f.write_str("4"),
+            Scale::X8 => f.write_str("8"),
+        }
+    }
+}
+
+impl_display_reg!(
+    IndexReg32 {
+        Eax => "eax",
+        Ebx => "ebx",
+        Ecx => "ecx",
+        Edx => "edx",
+        Esi => "esi",
+        Edi => "edi",
+        Ebp => "ebp",
+    }
+);
+
+impl_display_reg!(
+    IndexRex32 {
+        Eax => "eax",
+        Ebx => "ebx",
+        Ecx => "ecx",
+        Edx => "edx",
+        Esi => "esi",
+        Edi => "edi",
+        Ebp => "ebp",
+        R8d => "r8d",
+        R9d => "r9d",
+        R10d => "r10d",
+        R11d => "r11d",
+        R12d => "r12d",
+        R13d => "r13d",
+        R14d => "r14d",
+        R15d => "r15d",
+    }
+);
+
+impl_display_reg!(
+    IndexReg64 {
+        Rax => "rax",
+        Rbx => "rbx",
+        Rcx => "rcx",
+        Rdx => "rdx",
+        Rsi => "rsi",
+        Rdi => "rdi",
+        Rbp => "rbp",
+    }
+);
+
+impl_display_reg!(
+    IndexRex64 {
+        Rax => "rax",
+        Rbx => "rbx",
+        Rcx => "rcx",
+        Rdx => "rdx",
+        Rsi => "rsi",
+        Rdi => "rdi",
+        Rbp => "rbp",
+        R8 => "r8",
+        R9 => "r9",
+        R10 => "r10",
+        R11 => "r11",
+        R12 => "r12",
+        R13 => "r13",
+        R14 => "r14",
+        R15 => "r15",
+    }
+);
+
+impl<Base, Index> Display for Offset<Base, Index>
+where Base: Display + Copy, Index: Display + Copy {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match *self {
+            Offset::Disp(d) => write!(f, "{}", d),
+            Offset::Index(i, s) => write!(f, "{} * {}", i, s),
+            Offset::IndexDisp(i, s, d) => write!(f, "{} * {} + {}", i, s, d),
+            Offset::Base(b) => write!(f, "{}", b),
+            Offset::BaseDisp(b, d) => write!(f, "{} + {}", b, d),
+            Offset::BaseIndex(b, i, s) => write!(f, "{} + {} * {}", b, i, s),
+            Offset::BaseIndexDisp(b, i, s, d) => write!(f, "{} + {} * {} + {}", b, i, s, d),
+            Offset::RipDisp(d) => write!(f, "rip + {:#08x}", d),
+        }
+    }
+}
+
+impl_display_reg!(
+    Sreg {
+        Cs => "cs",
+        Ds => "ds",
+        Ss => "ss",
+        Es => "es",
+        Fs => "fs",
+        Gs => "gs",
+    }
+);
+
+impl<Base32, Index32, Base64, Index64> Display for Memory<Base32, Index32, Base64, Index64>
+where Offset<Base32, Index32>: Display, Offset<Base64, Index64>: Display {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        match *self {
+            Memory::Offset32(None, ref offset) => write!(f, "[{}]", offset),
+            Memory::Offset32(Some(sreg), ref offset) => write!(f, "[{}:{}]", sreg, offset),
+            Memory::Offset64(None, ref offset) => write!(f, "[{}]", offset),
+            Memory::Offset64(Some(sreg), ref offset) => write!(f, "[{}:{}]", sreg, offset),
+        }
+    }
+}
