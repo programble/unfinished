9ce3a8307c952b8b17710f4cdbb8127bf69e1101
Curtis McEnroe <programble@gmail.com>
2017-02-17 23:07:41 -0500

    Encode REX.W

--- src/code/encode.rs
+++ src/code/encode.rs
@@ -1,7 +1,13 @@
-use code::{Prefix3, Opcode, Imm, Instruction};
+use code::{Prefix3, Rex, Opcode, Imm, Instruction};
 
 use mnemonic::instruction::Adc;
 use mnemonic::operand::{Imm8, Imm16, Imm32};
+
+const REX: u8   = 0b0100_0000;
+const REX_W: u8 = 0b0100_1000;
+const REX_R: u8 = 0b0100_0100;
+const REX_X: u8 = 0b0100_0010;
+const REX_B: u8 = 0b0100_0001;
 
 impl Instruction {
     #[inline]
@@ -23,6 +29,12 @@ impl Instruction {
     #[inline]
     pub fn operand_size(mut self) -> Self {
         self.prefix3 = Some(Prefix3::OperandSize);
+        self
+    }
+
+    #[inline]
+    pub fn rex_w(mut self) -> Self {
+        self.rex = Some(self.rex.map_or(Rex(REX_W), |Rex(x)| Rex(x | REX_W)));
         self
     }
 
@@ -57,6 +69,7 @@ impl<'a> From<&'a Adc> for Instruction {
             AlImm8(imm) => Instruction::opcode1(0x14).imm8(imm),
             AxImm16(imm) => Instruction::opcode1(0x15).operand_size().imm16(imm),
             EaxImm32(imm) => Instruction::opcode1(0x15).imm32(imm),
+            RaxImm32(imm) => Instruction::opcode1(0x15).rex_w().imm32(imm),
             _ => unimplemented!(),
         }
     }
